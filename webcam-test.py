dce5db76a3d9461758e11fe4ebeb4b63670e6fd7
Seaw24 <ducnam883@gmail.com>
2026-01-20 21:51:06 -0700

    fix cv

--- webcam-test.py
+++ webcam-test.py
@@ -3,13 +3,14 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-    suscess,frame = cap.read()
+    suscess, frame = cap.read()
     if not suscess:
         print("Failed to grab frame")
         break
-    
+
     cv2.line(frame, (0, 400), (640, 400), (0, 255, 0), 4)
-    cv2.putText("Deadlift threshould ")
+    cv2.putText(frame, "VinaFit HUD Active", (20, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
     cv2.imshow("Vinafit live", frame)
 
